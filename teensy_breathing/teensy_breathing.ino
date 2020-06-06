30384ab7703854d79eb75cf950aa783b475e9359
Hal Avisado <hal@Hals-MacBook-Pro.local>
2020-06-06 16:30:13 +0900

    cleanup

--- teensy_breathing/teensy_breathing.ino
+++ teensy_breathing/teensy_breathing.ino
@@ -1,27 +1,3 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
 #define CYCLE_MS 500
 #define MAX_DUTY_CYCLE 100
 #define MIN_DUTY_CYCLE 2
@@ -83,9 +59,5 @@ void delayLoop(bool isBreatheIn, bool isLEDon)
   Serial.print(dutyCyclePercent);
   Serial.print(" ");
   Serial.println(calculatedDelay);
-  //  Serial.println(dutyCyclePercent);
-  //  Serial.print("delay: ");
-  // Serial.println(calculatedDelay);
-  // delay(100);
   delay(calculatedDelay);
 }
